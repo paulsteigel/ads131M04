60df2b8ee00a75f6f35f27b9cc4e242cc989ec68
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-20 03:27:27 +0700

    Update ads131m04_sensor.h

--- components/ads131m04/sensor/ads131m04_sensor.h
+++ components/ads131m04/sensor/ads131m04_sensor.h
@@ -21,13 +21,21 @@ public:
   ACValues sample(); // Modified return type
   void dump_config() override;
   void set_gain(ADS131M04Gain gain) { this->gain_ = gain; }
-  //ADS131M04Gain get_gain() { return this->gain_; }
   void set_channel(uint8_t channel);
+  id set_voltage_offset(float offset);
+  void set_voltage_scale(float scale);
+  void set_current_offset(float offset);
+  void set_current_scale(float scale);
 
 protected:
+  float calculateRMS(const int32_t* samples, int numSamples, float offset, float scale);
   //ADS131M04Gain gain_ = ADS131M04Gain::ADS131M04_GAIN_1; // Initialize with a default gain
   ADS131M04Gain gain_{ADS131M04_GAIN_1};
   uint8_t channel_; // Add channel_ member variable
+  float voltage_offset_ = 0.0f;
+  float voltage_scale_ = 1.0f;
+  float current_offset_ = 0.0f;
+  float current_scale_ = 1.0f;
 };
 
 }  // namespace ads131m04
