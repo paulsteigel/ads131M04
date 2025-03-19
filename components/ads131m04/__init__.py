8155b908773ddcb709d77e52a3c79d132c00a669
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 13:19:16 +0700

    Update __init__.py

--- components/ads131m04/__init__.py
+++ components/ads131m04/__init__.py
@@ -9,6 +9,8 @@ DEPENDENCIES = ["spi"]
 MULTI_CONF = True
 
 CONF_ADS131M04_ID = "ads131m04_id"
+CONF_DATA_READY_PIN = "data_ready_pin"
+CONF_RESET_PIN = "reset_pin"
 
 ads131m04_ns = cg.esphome_ns.namespace("ads131m04")
 ADS131M04 = ads131m04_ns.class_("ADS131M04", cg.Component, spi.SPIDevice)
@@ -17,19 +19,20 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(ADS131M04),
 		cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-		cv.Optional(CONF_DRDY_PIN): pins.gpio_input_pin_schema,
+		cv.Optional(CONF_DATA_READY_PIN): pins.gpio_input_pin_schema,
     }
 ).extend(spi.spi_device_schema(cs_pin_required=True))
 
 
 async def to_code(config):
-	var = cg.new_Pvariable(config[CONF_ID])
-	await cg.register_component(var, config)
-	await spi.register_spi_device(var, config)	
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await spi.register_spi_device(var, config)	
 	if CONF_RESET_PIN in config:
-		reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
-		cg.add(var.set_reset_pin(reset))
+        reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
+        cg.add(var.set_reset_pin(reset))
+
+	if CONF_DATA_READY_PIN in config:
+        reset = await cg.gpio_pin_expression(config[CONF_DATA_READY_PIN])
+        cg.add(var.set_data_ready_pin(reset))
 	
-	if CONF_DRDY_PIN in config:
-		reset = await cg.gpio_pin_expression(config[CONF_DRDY_PIN])
-		cg.add(var.set_data_ready_pin(reset))
