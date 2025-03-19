28bdeb1addf6330c9e32eb21bb84035a2a1aafd4
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 13:16:35 +0700

    Update __init__.py

--- components/ads131m04/__init__.py
+++ components/ads131m04/__init__.py
@@ -23,13 +23,13 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await spi.register_spi_device(var, config)	
+	var = cg.new_Pvariable(config[CONF_ID])
+	await cg.register_component(var, config)
+	await spi.register_spi_device(var, config)	
 	if CONF_RESET_PIN in config:
-	        reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
-	        cg.add(var.set_reset_pin(reset))
-
+		reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
+		cg.add(var.set_reset_pin(reset))
+	
 	if CONF_DRDY_PIN in config:
-	        reset = await cg.gpio_pin_expression(config[CONF_DRDY_PIN])
-	        cg.add(var.set_data_ready_pin(reset))
+		reset = await cg.gpio_pin_expression(config[CONF_DRDY_PIN])
+		cg.add(var.set_data_ready_pin(reset))
