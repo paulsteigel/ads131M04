dae4531fe75b7f07d99af608db0f1b93e5a26c5a
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 13:59:58 +0700

    Update ads131m04_sensor.cpp

--- components/ads131m04/sensor/ads131m04_sensor.cpp
+++ components/ads131m04/sensor/ads131m04_sensor.cpp
@@ -1,23 +1,29 @@
 #include "ads131m04_sensor.h"
+
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace ads131m04_sensor {
+namespace ads131m04 {
 
-static const char *const TAG = "ads131m04_sensor";
+static const char *const TAG = "ads131m04.sensor";
+
+void ADS131M04Sensor::dump_config() {
+  LOG_SENSOR("  ", "ADS131M04 Sensor", this);
+  ESP_LOGCONFIG(TAG, "    Multiplexer: %u", this->multiplexer_);
+  ESP_LOGCONFIG(TAG, "    Gain: %u", this->gain_);
+}
+
+float ADS131M04Sensor::sample() {
+  return this->parent_->request_measurement(this->multiplexer_, this->gain_, this->temperature_mode_);
+}
 
 void ADS131M04Sensor::update() {
-  if (this->parent_->data_ready_) {
-    if (this->channel_ >= 1 && this->channel_ <= 4) {
-      if (this->is_voltage_) {
-        this->publish_state(this->parent_->voltages_[this->channel_ - 1]);
-      } else {
-        this->publish_state(this->parent_->currents_[this->channel_ - 1]);
-      }
-    }
-    this->parent_->data_ready_ = false;
+  float v = this->sample();
+  if (!std::isnan(v)) {
+    ESP_LOGD(TAG, "'%s': Got Voltage=%fV", this->get_name().c_str(), v);
+    this->publish_state(v);
   }
 }
 
-} // namespace ads131m04_sensor
-} // namespace esphome
+}  // namespace ads131m04
+}  // namespace esphome
