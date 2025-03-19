ff67bfa4dceeb8465912dfd2222d906b3fbe8235
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 13:09:55 +0700

    Update __init__.py

--- components/ads131m04/__init__.py
+++ components/ads131m04/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import spi, sensor
+from esphome.components import spi
 from esphome.const import (
     CONF_ID,
     CONF_VOLTAGE,
@@ -13,58 +13,41 @@ from esphome.const import (
 )
 from esphome import pins
 
-CODEOWNERS = ["@solomondg1"]
+
+CODEOWNERS = ["@paulsteigel"]
 DEPENDENCIES = ["spi"]
 MULTI_CONF = True
 
 CONF_ADS131M04_ID = "ads131m04_id"
+CONF_DRDY_PIN = "data_ready_pin"
 CONF_RESET_PIN = "reset_pin"
-CONF_CHANNEL = "channel"
+CONF_SAMPLING_RATE = "sampling_rate"
+DEFAULT_SAMPLING_RATE = 16000
 
 ads131m04_ns = cg.esphome_ns.namespace("ads131m04")
 ADS131M04 = ads131m04_ns.class_("ADS131M04", cg.Component, spi.SPIDevice)
 
-CHANNEL_CONFIG = cv.Schema({
-    cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=4),
-})
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(ADS131M04),
-            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_VOLTAGE): cv.ensure_list(sensor.sensor_schema(
-                unit_of_measurement=UNIT_VOLT,
-                accuracy_decimals=3,
-                device_class=DEVICE_CLASS_VOLTAGE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ).extend(CHANNEL_CONFIG)),
-            cv.Optional(CONF_CURRENT): cv.ensure_list(sensor.sensor_schema(
-                unit_of_measurement=UNIT_AMPERE,
-                accuracy_decimals=3,
-                device_class=DEVICE_CLASS_CURRENT,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ).extend(CHANNEL_CONFIG)),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(spi.spi_device_schema(cs_pin_required=True))
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(ADS131M04),
+		cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+		cv.Optional(CONF_DRDY_PIN): pins.gpio_input_pin_schema,
+    }
 )
+.extend(cv.COMPONENT_SCHEMA)
+.extend(spi.spi_device_schema(cs_pin_required=True))
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-
-    if CONF_RESET_PIN in config:
+	
+	if CONF_RESET_PIN in config:
         reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
         cg.add(var.set_reset_pin(reset))
 
-    for conf in config.get(CONF_VOLTAGE, []):
-        sens = await sensor.new_sensor(conf)
-        cg.add(var.set_channel_voltage_sensor(conf[CONF_CHANNEL], sens))
-
-    for conf in config.get(CONF_CURRENT, []):
-        sens = await sensor.new_sensor(conf)
-        cg.add(var.set_channel_current_sensor(conf[CONF_CHANNEL], sens))
+	if CONF_DRDY_PIN in config:
+        reset = await cg.gpio_pin_expression(config[CONF_DRDY_PIN])
+        cg.add(var.set_data_ready_pin(reset))
+	
