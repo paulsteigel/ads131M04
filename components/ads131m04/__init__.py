34080ec78656e1744e255dc966f8b73050ff4b8b
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 12:21:18 +0700

    Update __init__.py

--- components/ads131m04/__init__.py
+++ components/ads131m04/__init__.py
@@ -13,39 +13,44 @@ from esphome.const import (
 )
 from esphome import pins
 
-DEPENDENCIES = ['spi']
-AUTO_LOAD = ['sensor']
+CODEOWNERS = ["@solomondg1"]
+DEPENDENCIES = ["spi"]
+MULTI_CONF = True
 
+CONF_ADS131M04_ID = "ads131m04_id"
 CONF_RESET_PIN = "reset_pin"
 CONF_CHANNEL = "channel"
 
-ads131m04_ns = cg.esphome_ns.namespace('ads131m04')
-ADS131M04 = ads131m04_ns.class_('ADS131M04', cg.Component, spi.SPIDevice)
+ads131m04_ns = cg.esphome_ns.namespace("ads131m04")
+ADS131M04 = ads131m04_ns.class_("ADS131M04", cg.Component, spi.SPIDevice)
 
 CHANNEL_CONFIG = cv.Schema({
     cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=4),
 })
 
 CONFIG_SCHEMA = (
-    cv.Schema({
-        cv.GenerateID(): cv.declare_id(ADS131M04),
-        cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_VOLTAGE): cv.ensure_list(sensor.sensor_schema(
-            unit_of_measurement=UNIT_VOLT,
-            accuracy_decimals=3,
-            device_class=DEVICE_CLASS_VOLTAGE,
-            state_class=STATE_CLASS_MEASUREMENT,
-        ).extend(CHANNEL_CONFIG)),
-        cv.Optional(CONF_CURRENT): cv.ensure_list(sensor.sensor_schema(
-            unit_of_measurement=UNIT_AMPERE,
-            accuracy_decimals=3,
-            device_class=DEVICE_CLASS_CURRENT,
-            state_class=STATE_CLASS_MEASUREMENT,
-        ).extend(CHANNEL_CONFIG)),
-    })
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(ADS131M04),
+            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_VOLTAGE): cv.ensure_list(sensor.sensor_schema(
+                unit_of_measurement=UNIT_VOLT,
+                accuracy_decimals=3,
+                device_class=DEVICE_CLASS_VOLTAGE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ).extend(CHANNEL_CONFIG)),
+            cv.Optional(CONF_CURRENT): cv.ensure_list(sensor.sensor_schema(
+                unit_of_measurement=UNIT_AMPERE,
+                accuracy_decimals=3,
+                device_class=DEVICE_CLASS_CURRENT,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ).extend(CHANNEL_CONFIG)),
+        }
+    )
     .extend(cv.COMPONENT_SCHEMA)
-    .extend(spi.spi_device_schema())
+    .extend(spi.spi_device_schema(cs_pin_required=True))
 )
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
