22aa3763a57fbbffbac1a8174deaab64a4ac111e
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-20 00:52:52 +0700

    Update __init__.py

--- components/ads131m04/sensor/__init__.py
+++ components/ads131m04/sensor/__init__.py
@@ -39,6 +39,18 @@ GAIN = {
     "128": ads131m04_ns.ADS131M04Gain.ADS131M04_GAIN_128, # Corrected namespace
 }
 
+#ADS131M04Gain = ads131m04_ns.enum("ADS131M04Gain")
+#GAIN = {
+#    "1": ADS131M04Gain.ADS131M04_GAIN_1,
+#    "2": ADS131M04Gain.ADS131M04_GAIN_2,
+#    "4": ADS131M04Gain.ADS131M04_GAIN_4,
+#    "8": ADS131m04Gain.ADS131M04_GAIN_8,
+#    "16": ADS131M04Gain.ADS131M04_GAIN_16,
+#    "32": ADS131M04Gain.ADS131M04_GAIN_32,
+#    "64": ADS131M04Gain.ADS131M04_GAIN_64,
+#    "128": ADS131M04Gain.ADS131M04_GAIN_128,
+#}
+
 ADS131M04Sensor = ads131m04_ns.class_(
     "ADS131M04Sensor",
     cg.PollingComponent,
