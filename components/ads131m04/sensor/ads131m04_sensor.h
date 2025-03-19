137029a776a0ba10b6e25e0b1de1d835e9446d4b
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-20 03:08:09 +0700

    Update ads131m04_sensor.h

--- components/ads131m04/sensor/ads131m04_sensor.h
+++ components/ads131m04/sensor/ads131m04_sensor.h
@@ -8,12 +8,17 @@
 namespace esphome {
 namespace ads131m04 {
 
+struct ACValues {
+  float voltage;
+  float current;
+};
+
 class ADS131M04Sensor : public PollingComponent,
                          public sensor::Sensor,
                          public Parented<ADS131M04> {
 public: 
   void update() override;
-  float sample(); // Removed 'override'
+  ACValues sample(); // Modified return type
   void dump_config() override;
   void set_gain(ADS131M04Gain gain) { this->gain_ = gain; }
   //ADS131M04Gain get_gain() { return this->gain_; }
