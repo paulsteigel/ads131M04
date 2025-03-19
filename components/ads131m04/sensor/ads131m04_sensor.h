f2f32a49d056481bd80aa6d7e10f721c715a006d
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 21:33:04 +0700

    Update ads131m04_sensor.h

--- components/ads131m04/sensor/ads131m04_sensor.h
+++ components/ads131m04/sensor/ads131m04_sensor.h
@@ -4,7 +4,6 @@
 #include "esphome/core/helpers.h"
 
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/voltage_sampler/voltage_sampler.h"
 
 #include "../ads131m04.h"
 
@@ -12,24 +11,38 @@ namespace esphome {
 namespace ads131m04 {
 
 class ADS131M04Sensor : public PollingComponent,
-                      public sensor::Sensor,
-                      public voltage_sampler::VoltageSampler,
-                      public Parented<ADS131M04> {
- public:
-  void update() override;
+                         public sensor::Sensor,
+                         public Parented<ADS131M04> {
+public:
+  /**
+   * @brief Constructor for ADS131M04 sensor.
+   *
+   * @param parent Pointer to the ADS131M04 parent component.
+   * @param sensor_num Sensor number (1-4).
+   * @param multiplexer Multiplexer setting (string).
+   * @param gain Gain setting (string).
+   */
+  ADS131M04Sensor(ADS131M04 *parent, uint8_t sensor_num, std::string multiplexer, std::string gain)
+      : PollingComponent(1000), // Default polling interval (1 second)
+        sensor::Sensor(),
+        Parented(parent),
+        sensor_num_(sensor_num),
+        multiplexer_(multiplexer),
+        gain_(gain) {}
 
-  void set_multiplexer(ADS131M04Multiplexer multiplexer) { this->multiplexer_ = multiplexer; }
-  void set_gain(ADS131M04Gain gain) { this->gain_ = gain; }
-  void set_temperature_mode(bool temp) { this->temperature_mode_ = temp; }
+  void update() override;
 
   float sample() override;
 
   void dump_config() override;
 
- protected:
-  ADS131M04Multiplexer multiplexer_{ADS131M04_MULTIPLEXER_P0_NG};
-  ADS131M04Gain gain_{ADS131M04_GAIN_6P144};
-  bool temperature_mode_;
+protected:
+  /** Sensor number (1-4). */
+  uint8_t sensor_num_;
+  /** Multiplexer setting (string). */
+  std::string multiplexer_;
+  /** Gain setting (string). */
+  std::string gain_;
 };
 
 }  // namespace ads131m04
