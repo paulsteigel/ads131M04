21ff4e62a7c88973834b0d30749a77dc49b908e7
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-20 00:30:32 +0700

    Update __init__.py

--- components/ads131m04/sensor/__init__.py
+++ components/ads131m04/sensor/__init__.py
@@ -3,7 +3,6 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     CONF_GAIN,
-    CONF_MULTIPLEXER,
     DEVICE_CLASS_VOLTAGE,
     STATE_CLASS_MEASUREMENT,
     UNIT_VOLT,
@@ -11,38 +10,35 @@ from esphome.const import (
 )
 from .. import ads131m04_ns, ADS131M04, CONF_ADS131M04_ID
 
-ADS131M04Multiplexer = ads131m04_ns.enum("ADS131M04Multiplexer")
-MUX = {
-    "AIN0P_AIN0N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN0P_AIN0N,
-    "AIN1P_AIN1N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN1P_AIN1N,
-    "AIN2P_AIN2N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN2P_AIN2N,
-    "AIN3P_AIN3N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN3P_AIN3N,
-    "AIN0P_AIN1N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN0P_AIN1N,
-    "AIN1P_AIN2N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN1P_AIN2N,
-    "AIN2P_AIN3N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN2P_AIN3N,
-    "AIN3P_AIN0N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN3P_AIN0N,
-    "AIN0": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN0,
-    "AIN1": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN1,
-    "AIN2": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN2,
-    "AIN3": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN3,
-    "SHORTED": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_SHORTED,
-    "DC_TEST_POS": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_DC_TEST_POS,
-    "DC_TEST_NEG": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_DC_TEST_NEG,
-}
+DEPENDENCIES = ["ads131m04"]
+
+# --- Multiplexer Configuration Removed ---
+# Hardware configuration has fixed differential input pairs (AIN0P/N, AIN1P/N, etc.).
+# Therefore, multiplexer switching is not required, and related code has been removed.
+# ADS131M04Multiplexer and MUX definitions are no longer needed.
+# --- End Multiplexer Removal ---
+
+# ADS131M04Multiplexer = ads131m04_ns.enum("ADS131M04Multiplexer")
+# MUX = {
+#     "AIN0P_AIN0N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN0P_AIN0N,
+#     "AIN1P_AIN1N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN1P_AIN1N,
+#     "AIN2P_AIN2N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN2P_AIN2N,
+#     "AIN3P_AIN3N": ADS131M04Multiplexer.ADS131M04_MULTIPLEXER_AIN3P_AIN3N,
+#     # ... (other multiplexer options) ...
+# }
 
 ADS131M04Gain = ads131m04_ns.enum("ADS131M04Gain")
 GAIN = {
     "1": ADS131M04Gain.ADS131M04_GAIN_1,
     "2": ADS131M04Gain.ADS131M04_GAIN_2,
     "4": ADS131M04Gain.ADS131M04_GAIN_4,
-    "8": ADS131M04Gain.ADS131M04_GAIN_8,
+    "8": ADS131m04Gain.ADS131M04_GAIN_8,
     "16": ADS131M04Gain.ADS131M04_GAIN_16,
     "32": ADS131M04Gain.ADS131M04_GAIN_32,
     "64": ADS131M04Gain.ADS131M04_GAIN_64,
     "128": ADS131M04Gain.ADS131M04_GAIN_128,
 }
 
-DEPENDENCIES = ["ads131m04"]
 ADS131M04Sensor = ads131m04_ns.class_(
     "ADS131M04Sensor",
     cg.PollingComponent,
@@ -64,8 +60,6 @@ CONFIG_SCHEMA = cv.typed_schema(
         .extend(
             {
                 cv.GenerateID(CONF_ADS131M04_ID): cv.use_id(ADS131M04),
-                cv.Required(CONF_MULTIPLEXER): cv.enum(MUX, upper=True, space="_"),
-                cv.Required(CONF_GAIN): cv.enum(GAIN, string=True),
             }
         )
         .extend(cv.polling_component_schema("60s")),
@@ -73,8 +67,25 @@ CONFIG_SCHEMA = cv.typed_schema(
     default_type=TYPE_ADC,
 )
 
+ADS131M04_SCHEMA = cv.Schema({
+    # --- Multiplexer Configuration Removed ---
+    # Removed CONF_MULTIPLEXER from the parent schema as multiplexer is fixed in hardware.
+    # cv.Optional(CONF_MULTIPLEXER): cv.enum(MUX, upper=True, space="_"),
+    # --- End Multiplexer Removal ---
+    cv.Optional(CONF_GAIN): cv.enum(GAIN, string=True),
+})
 
 async def to_code(config):
+    parent = await cg.get_variable(config[CONF_ADS131M04_ID])
+
+    # --- Multiplexer Configuration Removed ---
+    # Removed multiplexer setting logic from to_code as multiplexer is fixed in hardware.
+    # if CONF_MULTIPLEXER in config:
+    #     cg.add(parent.set_multiplexer(config[CONF_MULTIPLEXER]))
+    # --- End Multiplexer Removal ---
+
+    if CONF_GAIN in config:
+        cg.add(parent.set_gain(config[CONF_GAIN]))
     var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
     await cg.register_parented(var, config[CONF_ADS131M04_ID])
