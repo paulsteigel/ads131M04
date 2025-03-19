4856776bca660fce7837a8fbcaeb83da66167d38
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-20 02:57:53 +0700

    Update ads131m04.h

--- components/ads131m04/ads131m04.h
+++ components/ads131m04/ads131m04.h
@@ -329,8 +329,10 @@ public:
   float convert(int32_t datain);
   int32_t revconvert(float datain);
   int32_t twoscom(int32_t datain);
-  int32_t revtwoscom(int32_t datain);
-
+  int32_t revtwoscom(int32_t datain);  
+  int32_t ADS131M04::revconvert(float datain, ADS131M04Gain gain) {
+  float ADS131M04::convert(int32_t datain, ADS131M04Gain gain) {
+  
 protected:
   uint16_t config_{0};
   GPIOPin *data_ready_pin_{nullptr};  
