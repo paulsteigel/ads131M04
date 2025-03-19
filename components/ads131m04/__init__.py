aded8c209de1d9104733d81b49f8a7e47d9fbe84
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 22:55:34 +0700

    Update __init__.py

--- components/ads131m04/__init__.py
+++ components/ads131m04/__init__.py
@@ -1,7 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID
+from esphome.const import 
+	CONF_ID,
+	CONF_GAIN,
+    	CONF_MULTIPLEXER,
+    
 from esphome import pins
 
 CODEOWNERS = ["@paulsteigel"]
