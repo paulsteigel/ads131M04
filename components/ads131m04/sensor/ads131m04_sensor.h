a2983f22ae031ca16d2448028adc0711d980e420
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-03-19 12:26:29 +0700

    Update ads131m04_sensor.h

--- components/ads131m04/sensor/ads131m04_sensor.h
+++ components/ads131m04/sensor/ads131m04_sensor.h
@@ -2,10 +2,8 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
-
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/voltage_sampler/voltage_sampler.h"
-
 #include "../ads131m04.h"
 
 namespace esphome {
@@ -17,19 +15,16 @@ class ADS131M04Sensor : public PollingComponent,
                       public Parented<ADS131M04> {
  public:
   void update() override;
-
-  void set_multiplexer(ADS131M04Multiplexer multiplexer) { this->multiplexer_ = multiplexer; }
+  
+  void set_channel(uint8_t channel) { this->channel_ = channel; }
   void set_gain(ADS131M04Gain gain) { this->gain_ = gain; }
-  void set_temperature_mode(bool temp) { this->temperature_mode_ = temp; }
-
+  
   float sample() override;
-
   void dump_config() override;
 
  protected:
-  ADS131M04Multiplexer multiplexer_{ADS131M04_MULTIPLEXER_P0_NG};
-  ADS131M04Gain gain_{ADS131M04_GAIN_6P144};
-  bool temperature_mode_;
+  uint8_t channel_{0};  // Channel 0-3
+  ADS131M04Gain gain_{ADS131M04_GAIN_1};  // Default to unity gain
 };
 
 }  // namespace ads131m04
